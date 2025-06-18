19f75bb3bb0da071d0dcb4ef51d1c82e8cc8017c
ahmed-zai <saidahmed0703@gmail.com>
2025-06-18 10:04:05 +0100

    exerciseOne

---
+++ geekPython/weekOne/dayOne/exercise1.py
@@ -0,0 +1,14 @@
+#Exercise 1 : Hello World 
+
+#Print the following output in one line of code:
+
+#Hello world
+#Hello world
+#Hello world
+#Hello world
+i = 0 
+while i < 4 :
+    print("hello world")
+    i = i + 1
+
+
