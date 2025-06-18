c1aaeb230ca5982957206fb5b8ded53abcea8a17
ahmed-zai <saidahmed0703@gmail.com>
2025-06-18 10:14:07 +0100

    exercisetow

---
+++ geekPython/weekOne/dayOne/exercise2.py
@@ -0,0 +1,9 @@
+""" 
+ Exercise 2 : Some Math
+
+Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8) 
+ """
+powerResult = 99 ** 3 
+finResult = powerResult * 8 
+print(finResult)
+
