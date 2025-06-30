24c8b6577ca51a2c2689d275cef899d615690ede
ahmed-zai <saidahmed0703@gmail.com>
2025-06-30 13:46:42 +0100

    challenge

---
+++ geekSQL/weekTow/dayTow/challenge.sql
@@ -0,0 +1,22 @@
+-- Daily SQL Challenge – Puzzle Solution
+
+-- Table Creation and Inserts
+CREATE TABLE FirstTab (
+    id INTEGER,
+    name VARCHAR(10)
+);
+
+INSERT INTO FirstTab VALUES
+(5,'Pawan'),
+(6,'Sharlee'),
+(7,'Krish'),
+(NULL,'Avtaar');
+
+CREATE TABLE SecondTab (
+    id INTEGER
+);
+
+INSERT INTO SecondTab VALUES
+(5),
+(NULL);
+
