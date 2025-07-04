fdc53f110c39902eabe43b083b532f1cf5483ee7
ahmed-zai <saidahmed0703@gmail.com>
2025-07-04 12:25:10 +0100

    challengeStar

--- geekJS/dayTow/challengeStar.js
+++ geekJS/dayTow/challengeStar.js
@@ -17,13 +17,13 @@ Instructions
 
 
 
+
 // Using one loop
+let star = "";
 for (let i = 1; i <= 6; i++) {
-    let stars = '';
-    for (let j = 1; j <= i; j++) {
-        stars += '* ';
-    }
-    console.log(stars);
+    star += "* ";
+    console.log(star);
+
 }
 console.log('========================');
  // using tow nested for loops
