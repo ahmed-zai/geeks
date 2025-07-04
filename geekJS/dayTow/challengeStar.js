8e61381eb695c2614e7efa4b5cb7d7309af9ce54
ahmed-zai <saidahmed0703@gmail.com>
2025-07-04 12:21:07 +0100

    challenge

---
+++ geekJS/dayTow/challengeStar.js
@@ -0,0 +1,36 @@
+/*
+
+Instructions
+
+    Write a JavaScript program that recreates the pattern below.
+    Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+    Do this Daily Challenge by yourself, without looking at the answers on the internet.
+
+*  
+* *  
+* * *  
+* * * *  
+* * * * *
+* * * * * *
+
+*/
+
+
+
+// Using one loop
+for (let i = 1; i <= 6; i++) {
+    let stars = '';
+    for (let j = 1; j <= i; j++) {
+        stars += '* ';
+    }
+    console.log(stars);
+}
+console.log('========================');
+ // using tow nested for loops
+ for (let i = 1; i <= 6; i++) {
+    let line = '';
+    for (let j= 1; j <= i; j++) {
+        line += '* ';
+    }
+    console.log(line);
+ }
